86329dae22d9e6379685e3df72962bfa9fb8608d
tonylomaxtk <tony.lomax@travelperk.com>
2024-04-19 14:42:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import { createServer } from "http";
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  res.end("Hello, World!\n");
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
